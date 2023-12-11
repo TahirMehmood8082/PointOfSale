cd23a096a892965f8d3e22b30ffad7014d3f39f0
TahirMehmood8082 <infinitidev31aug22@gmail.com>
2023-12-11 15:20:20 +0500

    Close#3 01-firebase basic setup

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const environment = process.env.NODE_ENV
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>App is working! rr</Text>
       <StatusBar style="auto" />
     </View>
   );
